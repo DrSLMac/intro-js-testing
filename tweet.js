5db4d13346b8e013997bcf0236351767aeeb8492
Shauna MacFarlane <shauna.macfarlane579@gmail.com>
2022-05-26 18:59:05 -0500

    initial commit

--- tweet.js
+++ tweet.js
@@ -1,5 +1,24 @@
 class Tweet {
-
+  constructor(userName, userContent, userLink) {
+    this.sender = userName;
+    this.content = userContent;
+//allow to contain a link,
+//and if no link provided, return "no link provided"
+    this.link = userLink || 'no link provided';
+    this.likes = 0;
+    this.retweets = 0;
+    this.replies = [];
+    this.replyCount = 0;
+    this.retweets = 0;
+  }
+  like() {
+    this.likes++
+  }
+  retweet() {
+    this.retweets++
+  }
 }
+
+
 
 module.exports = Tweet;
